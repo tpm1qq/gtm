70927ba6c2f61887c675e6b21f2a4d57d451ee4a
tpm1qq <pkcyberpegasus@gmail.com>
2025-11-30 18:07:22 +0100

    added global flag and functionality

--- internal/app/gtm.go
+++ internal/app/gtm.go
@@ -8,33 +8,76 @@ import (
 	"github.com/tpm1qq/gtm/internal/pkg/tools/waybar"
 	"os"
 	"strings"
+	"sync"
 )
 
 func RunGTM() {
 	var tool string
+	var global bool
 	var color string
+	flag.BoolVar(&global, "global", false, "global flag; apply config changes to all tools at the same time")
+	flag.BoolVar(&global, "g", false, "global flag; apply config changes to all tools at the same time")
 	flag.StringVar(&tool, "tool", "", "which tool's config the user wants to change")
 	flag.StringVar(&tool, "t", "", "which tool's config the user wants to change")
 	flag.StringVar(&color, "color", "", "change the color of the given tool(s)")
 	flag.Parse()
 
 	tool = strings.ToLower(tool)
-	switch tool {
-	case "hyprland":
-		if err := hyprland.Hyprland_SetColor(color); err != nil {
-			fmt.Fprintln(os.Stderr, "error setting color:", err)
+	switch {
+	case global && tool == "":
+		var wg = sync.WaitGroup{}
+		var errors = make(chan error, 4)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if err := hyprland.Hyprland_SetColor(color); err != nil {
+				errors <- err
+			}
+		}()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if err := waybar.Waybar_SetColor(color); err != nil {
+				errors <- err
+			}
+			if err := waybar.Waybar_reload(); err != nil {
+				errors <- err
+			}
+		}()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if err := rofi.Rofi_SetColor(color); err != nil {
+				errors <- err
+			}
+		}()
+		wg.Wait()
+		close(errors)
+		for err := range errors {
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "error setting color:", err)
+			}
 		}
-	case "waybar":
-		if err := waybar.Waybar_SetColor(color); err != nil {
-			fmt.Fprintln(os.Stderr, "error setting color:", err)
-		}
-		if err := waybar.Waybar_reload(); err != nil {
-			fmt.Fprintln(os.Stderr, "error reloading waybar", err)
-		}
+	case !global && tool != "":
+		switch tool {
+		case "hyprland":
+			if err := hyprland.Hyprland_SetColor(color); err != nil {
+				fmt.Fprintln(os.Stderr, "error setting color:", err)
+			}
+		case "waybar":
+			if err := waybar.Waybar_SetColor(color); err != nil {
+				fmt.Fprintln(os.Stderr, "error setting color:", err)
+			}
+			if err := waybar.Waybar_reload(); err != nil {
+				fmt.Fprintln(os.Stderr, "error reloading waybar", err)
+			}
 
-	case "rofi":
-		if err := rofi.Rofi_SetColor(color); err != nil {
-			fmt.Fprintln(os.Stderr, "error setting color:", err)
+		case "rofi":
+			if err := rofi.Rofi_SetColor(color); err != nil {
+				fmt.Fprintln(os.Stderr, "error setting color:", err)
+			}
 		}
+	case !global && tool == "":
+		fmt.Println("neither global nor tool flag set!")
 	}
 }
