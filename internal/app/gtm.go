03eee9e34cb7951242332cfeab1ac7e354fee884
tpm1qq <pkcyberpegasus@gmail.com>
2025-12-08 20:22:21 +0100

    added theme functionality and sample inserts/themes

--- internal/app/gtm.go
+++ internal/app/gtm.go
@@ -3,34 +3,24 @@ package app
 import (
 	"flag"
 	"fmt"
-	"github.com/tpm1qq/gtm/internal/pkg/core"
 	"os"
+	"path/filepath"
 	"sync"
+
+	"github.com/tpm1qq/gtm/internal/pkg/core"
+	t "github.com/tpm1qq/gtm/internal/pkg/theme"
 )
 
 func RunGTM() {
+	var homeDir, _ = os.UserHomeDir()
+	var cfgDir = filepath.Join(homeDir, ".config", "gtm", "themes")
 	var tools core.ToolList
+	var theme string
 	var global bool
 	var color string
 	var wallpaper string
 	var settings core.ToolSettings
 	var selection core.ToolList
-
-	flag.BoolVar(&global, "global", false, "global flag; apply config changes to all tools at the same time")
-	flag.BoolVar(&global, "g", false, "global flag; apply config changes to all tools at the same time")
-	flag.Var(&tools, "package", "which tool's config the user wants to change")
-	flag.Var(&tools, "p", "which tool's config the user wants to change")
-	flag.StringVar(&color, "color", "", "change the color of the given tool(s)")
-	flag.StringVar(&wallpaper, "wallpaper", "", "change the current wallpaper")
-	flag.StringVar(&wallpaper, "w", "", "change the current wallpaper")
-
-	flag.Parse()
-
-	settings = core.ToolSettings{
-		Color:     color,
-		Wallpaper: wallpaper,
-	}
-
 	var all = core.ToolList{
 		core.ToolHyprland,
 		core.ToolWaybar,
@@ -38,18 +28,66 @@ func RunGTM() {
 		core.ToolHyprpaper,
 	}
 
-	switch {
-	case global && len(tools) == 0:
-		selection = all
-	case !global && len(tools) > 0:
-		selection = tools
-	case !global && len(tools) == 0:
-		fmt.Println("neither global nor tool flag set!")
-		return
-	default:
-		fmt.Println("flag error")
-		return
+	flag.BoolVar(&global, "global", false, "global flag; apply config changes to all tools at the same time")
+	flag.BoolVar(&global, "g", false, "global flag; apply config changes to all tools at the same time")
+	flag.Var(&tools, "tools", "which tool's config the user wants to change")
+	flag.StringVar(&color, "color", "", "change the color of the given tool(s)")
+	flag.StringVar(&wallpaper, "wallpaper", "", "change the current wallpaper")
+	flag.StringVar(&theme, "theme", "", "use a gtm theme")
+
+	flag.Parse()
+
+	if len(theme) > 0 {
+		themePath := filepath.Join(cfgDir, theme+".yaml")
+		v, err := t.LoadTheme(themePath)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "error loading theme:", err)
+			return
+		}
+		if v.Global.Enabled {
+			selection = all
+			settings = core.ToolSettings{
+				Color:     v.Global.Color,
+				Wallpaper: v.Global.Wallpaper,
+			}
+		} else {
+			settings = core.ToolSettings{
+				Color:     v.Tools.Hyprland.Color,
+				Wallpaper: v.Tools.Hyprpaper.Wallpaper,
+			}
+			if v.Tools.Hyprland.Enabled {
+				selection = append(selection, core.ToolHyprland)
+			}
+			if v.Tools.Hyprpaper.Enabled {
+				selection = append(selection, core.ToolHyprpaper)
+			}
+			if v.Tools.Waybar.Enabled {
+				selection = append(selection, core.ToolWaybar)
+			}
+			if v.Tools.Rofi.Enabled {
+				selection = append(selection, core.ToolRofi)
+			}
+		}
+	} else {
+		settings = core.ToolSettings{
+			Color:     color,
+			Wallpaper: wallpaper,
+		}
+
+		switch {
+		case global && len(tools) == 0:
+			selection = all
+		case !global && len(tools) > 0:
+			selection = tools
+		case !global && len(tools) == 0:
+			fmt.Println("neither global nor tool flag set!")
+			return
+		default:
+			fmt.Println("flag error")
+			return
+		}
 	}
+
 	var wg = sync.WaitGroup{}
 	var errs = make(chan error, len(selection))
 	for _, v := range selection {
---
+++ internal/pkg/theme/theme.go
@@ -0,0 +1,59 @@
+package theme
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Theme struct {
+	Global GlobalBlock `yaml:"global"`
+	Tools  ToolsBlock  `yaml:"tools"`
+}
+
+type GlobalBlock struct {
+	Enabled   bool   `yaml:"enabled"`
+	Wallpaper string `yaml:"wallpaper"`
+	Color     string `yaml:"color"`
+}
+
+type ToolsBlock struct {
+	Hyprland  HyprlandBlock  `yaml:"hyprland"`
+	Hyprpaper HyprpaperBlock `yaml:"hyprpaper"`
+	Waybar    WaybarBlock    `yaml:"waybar"`
+	Rofi      RofiBlock      `yaml:"rofi"`
+}
+
+type HyprlandBlock struct {
+	Enabled bool   `yaml:"enabled"`
+	Color   string `yaml:"color"`
+}
+
+type HyprpaperBlock struct {
+	Enabled   bool   `yaml:"enabled"`
+	Wallpaper string `yaml:"wallpaper"`
+}
+
+type WaybarBlock struct {
+	Enabled bool   `yaml:"enabled"`
+	Color   string `yaml:"color"`
+}
+
+type RofiBlock struct {
+	Enabled bool   `yaml:"enabled"`
+	Color   string `yaml:"color"`
+}
+
+func LoadTheme(p string) (Theme, error) {
+	data, err := os.ReadFile(p)
+	if err != nil {
+		return Theme{}, fmt.Errorf("error reading file: %w", err)
+	}
+	var r Theme
+	err = yaml.Unmarshal(data, &r)
+	if err != nil {
+		return Theme{}, fmt.Errorf("error yaml.Unmarshal: %w", err)
+	}
+	return r, nil
+}
